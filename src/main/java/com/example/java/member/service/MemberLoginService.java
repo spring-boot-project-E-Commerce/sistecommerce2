3f40d4455cc78e81e74f3762ffafdac8a31d8069
wauder0430 <wlaudaoq123@naver.com>
2026-06-01 22:27:53 +0900

    session 기초 로그인

--- src/main/java/com/example/java/member/service/MemberLoginService.java
+++ src/main/java/com/example/java/member/service/MemberLoginService.java
@@ -1,11 +1,15 @@
 package com.example.java.member.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.example.java.member.dto.MemberDto;
 import com.example.java.member.entity.Member;
 import com.example.java.member.repository.MemberRepository;
+
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
