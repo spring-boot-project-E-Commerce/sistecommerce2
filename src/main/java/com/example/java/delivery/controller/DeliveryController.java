25b4f8087ff3beb308e47ea1bfe89d63b1788a79
kmd192kmd <kmd192kmd@gmail.com>
2026-06-02 12:48:56 +0900

    식사전 임시 저장

--- src/main/java/com/example/java/delivery/controller/DeliveryController.java
+++ src/main/java/com/example/java/delivery/controller/DeliveryController.java
@@ -5,11 +5,22 @@ import org.springframework.stereotype.Controller;
 import com.example.java.delivery.service.DeliveryService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
+@RequestMapping("/shop")
 @RequiredArgsConstructor
 public class DeliveryController {
 
 	private final DeliveryService deliveryService;
+	
+	@GetMapping("/list")
+	public String list() {
+		
+		return "list";
+	}
 	
 }
