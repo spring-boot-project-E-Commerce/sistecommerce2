a8e3276651ad9343c21ca563f78c615076a1c2eb
wauder0430 <wlaudaoq123@naver.com>
2026-06-04 10:56:53 +0900

    feat: principal 의존성 추가

---
+++ src/main/java/com/example/java/member/service/MemberSecurityService.java
@@ -0,0 +1,31 @@
+package com.example.java.member.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.java.member.entity.Member;
+import com.example.java.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberSecurityService implements UserDetailsService{
+	
+	private final MemberRepository memberRepository;
+	
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 아이디입니다: " + username));
+
+        return User.builder()
+                .username(member.getUsername())
+                .password(member.getPassword())
+                .roles(member.getRole())
+                .build();
+	}
+
+}
