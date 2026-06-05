2474e50c992c438b652036ff4203612004031334
wauder0430 <wlaudaoq123@naver.com>
2026-06-05 17:39:28 +0900

    fix: 날짜 관련 null 오류 처리

--- src/main/java/com/example/java/member/entity/Member.java
+++ src/main/java/com/example/java/member/entity/Member.java
@@ -1,19 +1,13 @@
 package com.example.java.member.entity;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "member")
