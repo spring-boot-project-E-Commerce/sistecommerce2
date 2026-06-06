f36131fab4b5cfd471b9323c5e22eb9c29d7c4a5
JJanE2 <aaa1685@naver.com>
2026-06-06 16:40:59 +0900

    feat: implement update purchase-order feature

--- src/main/java/com/example/java/purchaseorder/controller/PurchaseOrderAdminController.java
+++ src/main/java/com/example/java/purchaseorder/controller/PurchaseOrderAdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.java.product.entity.Options;
-import com.example.java.product.repository.OptionsRepository;
+import com.example.java.product.service.OptionsService;
 import com.example.java.purchaseorder.dto.PurchaseOrderListDTO;
 import com.example.java.purchaseorder.entity.PurchaseOrder;
 import com.example.java.purchaseorder.service.PurchaseOrderService;
@@ -23,8 +23,7 @@ public class PurchaseOrderAdminController {
 
 	private final PurchaseOrderService purchaseOrderService;
 	// TODO OptionsService 생기면 수정해야 함
-//	private final OptionsService
-	private final OptionsRepository optionsRepository;
+	private final OptionsService optionsService;
 	
 	// TODO 임시 목록화면 (지금은 발주등록 이동위해 옵션 정보나오게 함, 나중에는 발주등록은 재고현황목록에서)
 	@GetMapping("/purchase-orders")
@@ -44,10 +43,8 @@ public class PurchaseOrderAdminController {
 	public String showCreateForm(@PathVariable(value = "optionsSeq") Long optionsSeq, Model model) {
 
 		// TODO null 오류처리 필요
-		Options options = optionsRepository.findById(optionsSeq).get();
+		Options options = optionsService.findById(optionsSeq);
 		
-		// TODO OptionsService 생기면 수정해야 함
-//	    Options option = optionsService.findById(optionsSeq);
 		// TODO Options 이 product랑 연결되면 수정해야 함
 		int supplyPrice = 200;
 
