5303e942189616894e559660b66ab9ead948ae09
JJanE2 <aaa1685@naver.com>
2026-06-11 11:08:35 +0900

    feat: implement group-buy create feature

--- src/main/java/com/example/java/admin/service/GroupBuyAdminService.java
+++ src/main/java/com/example/java/admin/service/GroupBuyAdminService.java
@@ -1,5 +1,6 @@
 package com.example.java.admin.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,8 +10,15 @@ import com.example.java.admin.dto.GroupBuyCreateDto;
 import com.example.java.admin.dto.GroupBuyOptionDto;
 import com.example.java.admin.dto.SellerInfoDto;
 import com.example.java.admin.repository.GroupBuyQueryDslRepository;
+import com.example.java.groupbuy.entity.GroupBuy;
+import com.example.java.groupbuy.entity.GroupBuyOptions;
+import com.example.java.groupbuy.entity.GroupBuyStatus;
+import com.example.java.groupbuy.repository.GroupBuyOptionsRepository;
+import com.example.java.groupbuy.repository.GroupBuyRepository;
 import com.example.java.product.entity.Options;
 import com.example.java.product.entity.Product;
+import com.example.java.product.repository.OptionsRepository;
+import com.example.java.product.repository.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +28,10 @@ import lombok.RequiredArgsConstructor;
 public class GroupBuyAdminService {
 
     private final GroupBuyQueryDslRepository repository;
+    private final GroupBuyRepository groupBuyRepository;
+    private final GroupBuyOptionsRepository groupBuyOptionsRepository;
+    private final ProductRepository productRepository;
+    private final OptionsRepository optionsRepository;
 
     public GroupBuyCreateDto getCreateForm(List<Long> optionSeqs) {
 
@@ -59,4 +71,40 @@ public class GroupBuyAdminService {
                 .originalPrice(product.getPrice())
                 .build();
     }
+    
+    @Transactional
+    public Long create(GroupBuyCreateDto dto) {
+    	Product product = productRepository.findById(dto.getProductSeq()).get();
+
+        GroupBuy groupBuy = GroupBuy.builder()
+                .product(product)
+                .startAt(dto.getStartAt())
+                .endAt(dto.getEndAt())
+                .minCount(dto.getMinCount())
+                .maxCount(dto.getMaxCount())
+                .createdAt(LocalDateTime.now())
+                .originalPrice(dto.getOriginalPrice())
+                .finalPrice(dto.getFinalPrice())
+                .status(GroupBuyStatus.SCHEDULED)
+                .build();
+
+        groupBuyRepository.save(groupBuy);
+
+        for (GroupBuyOptionDto option : dto.getOptions()) {
+        	Options options = optionsRepository.findById(option.getOptionSeq()).get();
+
+            GroupBuyOptions groupBuyOption =
+                    GroupBuyOptions.builder()
+                            .groupBuy(groupBuy)
+                            .options(options)
+                            .orderQty(option.getOrderQty())
+                            .occupiedCount(0)
+                            .build();
+
+            groupBuyOptionsRepository.save(groupBuyOption);
+        }
+
+        return groupBuy.getSeq();
+    }
+    
 }
